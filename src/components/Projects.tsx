2c169777240d3fb739c0a89f623503a609a47382
Kieran Hardwick <kieranhardwick260@gmail.com>
2023-08-06 22:12:15 +0100

    Init projects page

---
+++ src/components/Projects.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from '../style'
+
+const Projects = () => {
+  return (
+    <section className={`flex flex-col bg-background items-center w-screen h-[200px] ${styles.paddingX}`}>
+        <h1 className="text-white bg-clip-text text-2xl font-bold tracking-wide">Projects</h1>
+    </section>
+  )
+}
+
+export default Projects
